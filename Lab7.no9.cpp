1fd11e5a0d7e3722879cfd1a675a3e115e988dc7
Brain17 <b.daminov@newuu.uz>
2025-06-17 16:36:34 +0500

    Add files via upload

---
+++ Lab7.no9.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int print_numbers(int n) {
+    if (n==0) {
+        return 0;
+    }
+    cout<<n<<endl;
+        return print_numbers(n-1);
+}
+int main() {
+    int n;
+    cin>>n;
+    print_numbers(n);
+    return 0;
+}
